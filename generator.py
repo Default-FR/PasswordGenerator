7c5427e233ff7b46108978d7549a1a508895a43d
pBouillon <pierre.bouillon@openmailbox.org>
2018-02-07 14:44:30 +0100

    switched to py3

--- generator.py
+++ generator.py
@@ -1,15 +1,29 @@
-from random              import choice
-from random              import shuffle
-from random              import sample
+import random
+from random  import choice
+from random  import shuffle
+from random  import sample
 
+import generationException
 from generationException import LengthException
 
-'''
-    Generator object 
-'''
+SHUFFLE_LIM = 75
+
+MIN_SIZE = 8
+MAX_SIZE = 64
+
 class Generator():
+    """References the generator
+
+    Attributes:
+        __length  : password size
+        __maj     : contains majs
+        __num     : contains numbers
+        __char    : contains special chars
+        __genList : char list for the generation
+        __psswd   : generated password
+    """
     def __init__(self,length,maj,num,char):
-        self.__length  = length
+        self.__length  = int(length)
         self.__maj     = maj
         self.__num     = num
         self.__char    = char
@@ -18,62 +32,65 @@ class Generator():
         self.__genPsswd()
 
     def __str__(self):
-        objstr = \
-        "self.__length  =   "+ str(self.__length)+"\r\n"+\
-        "self.__maj     =   "+ str(self.__maj)+"\r\n"+\
-        "self.__num     =   "+ str(self.__num )+"\r\n"+\
-        "self.__char    =   "+ str(self.__char)+"\r\n"
-        return objstr
+        """Prints password and its generation details
+        """
+        psswdstr = "size  : {}\n"
+        psswdstr+= "maj   : {}\n"
+        psswdstr+= "num   : {}\n"
+        psswdstr+= "char  : {}\n"
+        psswdstr+= "\n"
+        psswdstr+= "final : {}\n"
+        return psswdstr.format(
+                self.__length,
+                self.__maj,
+                self.__num,
+                self.__char,
+                self.__psswd
+            )
 
-    '''
-        TL;DR   return a list for the password generation
-
-        init a list containing [a-z]
-        if self.__maj  -> add [A-Z]
-        if self.__num  -> add [0-9]
-        if self.__char -> add special chars
-
-        then shuffle the list
-    '''
     def __charList(self):
+        """Generate password generator's alphabet
+
+        First add each alphabet
+        Then shuffle the list
+
+        Returns:
+            lGen : list containing the alphabet
+        """
+
         lGen = [chr(97+x) for x in range(25)]
        
-        if self.__char == True :
+        if self.__char :
             lGen += [chr(33+x) for x in range(13)]
             lGen += [chr(58+x) for x in range(6)]
             lGen += [chr(91+x) for x in range(5)]
             lGen += [chr(123+x) for x in range(3)]
 
-        if self.__maj == True :
+        if self.__maj :
             lGen += [chr(65+x) for x in range(25)]
 
-        if self.__num == True :
+        if self.__num :
             lGen += [chr(48+x) for x in range(9)]
 
-        for x in xrange(75):
+        for x in range(SHUFFLE_LIM):
             shuffle(lGen)
+
         return lGen
         
-
-    '''
-        TL;DR   generate the password using self.__genList
-                raise LengthException
-
-        use the self.__genList to create the password
-
-        raise a LengthException if the password isn't in the
-        defined bounds (5<length<64)
-    '''
     def __genPsswd(self):
-        if int(self.__length)<5 or int(self.__length)>64:
+        """Generate the password
+
+        Check password size
+        Then add randomly a char until the max size
+        """
+        if not MIN_SIZE <= self.__length <= MAX_SIZE:
             raise LengthException
             system.exit(0)
 
-        for i in xrange(int(self.__length)):
+        for _ in range(self.__length):
             self.__psswd += choice(self.__genList)
 
-    '''
-        return string -> the password generated
-    '''
-    def getPsswd(self):
+    def get_password(self):
+        """Returns the generated password
+        """
         return self.__psswd
--- main.py
+++ main.py
@@ -1,46 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import generator
+from generator import MAX_SIZE
+from generator import MIN_SIZE
 from generator import Generator
+
+import getopt
 from getopt    import getopt
 from getopt    import GetoptError
+
+import sys
 from sys       import argv
 from sys       import exit
 
-def usage():
-    print "usage:"
-    print "   -l [length]        Enter the password length (4<l<32)"
-    print "   -c                 To enable special chars"
-    print "   -m                 To enable upper case"
-    print "   -n                 To enable numbers"
+DEFAULT_SIZE = 32
+EXIT_FAILURE = 1
+EXIT_SUCCESS = 0
+
+def print_usage(err_ret):
+    msg = 'usage:'
+    msg+= '\t-l [length] Enter the password length ({} < l < {})'
+    msg+= '\t-c          To enable special chars'
+    msg+= '\t-m          To enable upper case'
+    msg+= '\t-n          To enable numbers'
+    print (msg.format(MIN_SIZE, MAX_SIZE))
+    exit (err_ret)
 
 if __name__ == '__main__':
-
-    char   = False
-    length = 16
-    maj    = False
-    num    = False
+    char   = True
+    maj    = True
+    num    = True
+    length = DEFAULT_SIZE
 
     try:
-        opts, args = getopt(argv[1:], "chnml:", ["help", "output="])
-
+        opts, args = getopt (
+            argv[1:], 
+            'chnml:', 
+            ['help', 'output=']
+        )
     except GetoptError as err:
-        print str(err)
-        usage()
-        exit(2)
+        print(str(err))
+        print_usage (EXIT_FAILURE)
 
     for o, a in opts:
-        if o == "-c":
+        if o is '-c':
             char = True
-        elif o in ("-h", "--help"):
-            usage()
-            exit()
-        elif o in ("-l"):
+        elif o in ('-h', '--help'):
+            print_usage(EXIT_SUCCESS)
+        elif o in ('-l'):
             length = a
-        if o == "-m":
+        if o is '-m':
             maj = True
-        if o == "-n":
+        if o is '-n':
             num = True
 
-    g = Generator(length,maj,num,char)
-    print(g.getPsswd())
+    g = Generator (length, maj, num, char)
+    password = g.get_password()
+    # print(password)
+    # print('-----')
+    print(g)
